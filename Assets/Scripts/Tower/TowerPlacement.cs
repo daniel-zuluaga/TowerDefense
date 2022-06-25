b6a5f18adbb322ba973d275399140b471dfcc65f
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-06-25 15:52:29 -0500

    In this commit I created the explosion for the pipe

--- Assets/Scripts/Tower/TowerPlacement.cs
+++ Assets/Scripts/Tower/TowerPlacement.cs
@@ -45,7 +45,7 @@ public class TowerPlacement : MonoBehaviour
             else
             {
                 curSelectedTile = null;
-                previewTower.transform.position = new Vector3(0, 999, 0);
+                previewTower.transform.position = new Vector3(0, 10, 0);
             }
             if(Mouse.current.leftButton.isPressed && curSelectedTile != null && curSelectedTile.tower == null)
             {
